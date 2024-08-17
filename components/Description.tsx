dee1d8cb8e8978d26b37f9351f0e8ef0c873d0c9
Adeel Shabbir <145215661+jaatadeel14@users.noreply.github.com>
2024-08-17 15:16:24 +0300

    Update Description.tsx

--- components/Description.tsx
+++ components/Description.tsx
@@ -2,8 +2,10 @@
 const Description = () => {
   return (
     <div>
+      
         <p className="text-xl">UXservices is an international UX design and research company that provides consulting and trainings, organizes conferences and events, and publishes books and resources.
         We also help organizations to build Web3 and Metaverse editions of their businesses with frictionless user experiences.</p>
+      
     </div>
   )
 }
